39f3b20ad53f31c9049a74ecebb593d7e849575a
Davide <rinaldi.davide94@gmail.com>
2020-12-02 16:44:54 +0100

    Complete

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const api_key = '7ba085c4229baa855e3ee88a6b076287';
 const myApp = new Vue({
 	el: "#root",
 	data: {
-		searchInput: ''
+        searchInput: '',
+        movie: [],
 	},
 	methods:{
-        searchFilm: () => {
+        searchFilm: function() {
+            console.log(this.searchInput)
             axios
                 .get('https://api.themoviedb.org/3/search/movie',{
                     params: {
@@ -14,10 +16,11 @@ const myApp = new Vue({
                         query: this.searchInput,
                     }
                 })
-                .then(film => console.log(film) );
-                console.log(this.searchInput)
+                .then((r) => {this.movie = r.data.results})
+                .finally(() => this.searchInput = '')
         }
 	},
 	
 
 })
+
